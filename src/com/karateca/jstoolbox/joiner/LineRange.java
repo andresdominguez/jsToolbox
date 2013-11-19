943fbeb819e8cdcdbd1d22864d99a1bd734e537a
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-11-18 22:31:56 -0500

    new

---
+++ src/com/karateca/jstoolbox/joiner/LineRange.java
@@ -0,0 +1,22 @@
+package com.karateca.jstoolbox.joiner;
+
+/**
+ * @author Andres Dominguez.
+ */
+class LineRange {
+  private final int start;
+  private final int end;
+
+  public LineRange(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  public int getStart() {
+    return start;
+  }
+
+  public int getEnd() {
+    return end;
+  }
+}
