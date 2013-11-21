54c5c071dcdb75651aacc4f74a4cb044be3afc52
andresdom <andresdom@google.com>
2013-11-21 16:11:27 -0500

    new action to override

---
+++ src/com/karateca/jstoolbox/generatemethod/OverrideMethodAction.java
@@ -0,0 +1,14 @@
+package com.karateca.jstoolbox.generatemethod;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author andresdom@google.com (Andres Dominguez)
+ */
+public class OverrideMethodAction extends AnAction {
+
+  public void actionPerformed(AnActionEvent e) {
+    // TODO: insert action logic here
+  }
+}
