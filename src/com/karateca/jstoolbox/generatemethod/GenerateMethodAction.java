75dc1a691d15779f4c31e9a2ef03a3faa6f76de6
andresdom <andresdom@google.com>
2013-11-21 13:46:24 -0500

    add implementation

--- src/com/karateca/jstoolbox/generatemethod/GenerateMethodAction.java
+++ src/com/karateca/jstoolbox/generatemethod/GenerateMethodAction.java
@@ -1,18 +1,71 @@
 package com.karateca.jstoolbox.generatemethod;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.impl.DocumentImpl;
+import com.intellij.openapi.editor.impl.EditorImpl;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.karateca.jstoolbox.MyAction;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  * @author Andres Dominguez
  */
 public class GenerateMethodAction extends MyAction {
 
-  public void actionPerformed(AnActionEvent e) {
-    if (!canEnableAction(e)) {
+  private Project project;
+  private EditorImpl editor;
+  private DocumentImpl document;
+  private NamespaceFinder namespaceFinder;
+
+  public void actionPerformed(AnActionEvent actionEvent) {
+    if (!canEnableAction(actionEvent)) {
       return;
     }
 
+    project = actionEvent.getData(PlatformDataKeys.PROJECT);
+    editor = (EditorImpl) actionEvent.getData(PlatformDataKeys.EDITOR);
+    VirtualFile virtualFile = actionEvent
+        .getData(PlatformDataKeys.VIRTUAL_FILE);
+    document = (DocumentImpl) editor.getDocument();
 
+    namespaceFinder = new NamespaceFinder(project, document, virtualFile);
+
+    // Async callback to get the search results for it( and describe(
+    namespaceFinder.addResultsReadyListener(new ChangeListener() {
+      @Override
+      public void stateChanged(ChangeEvent changeEvent) {
+        if (changeEvent.getSource().equals("NamespaceFound")) {
+          String namespace = namespaceFinder.getNamespaceFound();
+
+          // TODO: show error: "not found"
+          if (namespace != null) {
+            addMethod(namespace);
+          }
+        }
+      }
+    });
+
+    namespaceFinder.findText("goog.provide", false);
+  }
+
+  private void addMethod(final String namespace) {
+    runWriteActionInsideCommand(project, "Added method", new Runnable() {
+      @Override
+      public void run() {
+        int offset = editor.getCaretModel().getOffset();
+
+        String methodTemplate = String.format("%s.prototype. = function() {\n" +
+            "  \n" +
+            "};", namespace);
+        document.replaceString(offset, offset, methodTemplate);
+
+        // Put the caret after "protoype."
+        editor.getCaretModel().moveToOffset(offset + namespace.length() + 11);
+      }
+    });
   }
 }
