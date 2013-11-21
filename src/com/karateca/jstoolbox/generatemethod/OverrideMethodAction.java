0196783a0fc3a6b838b5a5c2d522593fb1c0f888
andresdom <andresdom@google.com>
2013-11-21 16:50:45 -0500

    Add code to find the parent ns

--- src/com/karateca/jstoolbox/generatemethod/OverrideMethodAction.java
+++ src/com/karateca/jstoolbox/generatemethod/OverrideMethodAction.java
@@ -1,15 +1,113 @@
 package com.karateca.jstoolbox.generatemethod;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.components.JBList;
 import com.karateca.jstoolbox.MyAction;
+
+import java.util.List;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  * @author Andres Dominguez
  */
 public class OverrideMethodAction extends MyAction {
 
-  @Override
-  public void actionPerformed(AnActionEvent e) {
+  private Project project;
+  private Editor editor;
+  private Document document;
+  private ParentNamespaceFinder2 namespaceFinder;
 
+  @Override
+  public void actionPerformed(AnActionEvent actionEvent) {
+    if (!canEnableAction(actionEvent)) {
+      return;
+    }
+    project = actionEvent.getData(PlatformDataKeys.PROJECT);
+    editor = actionEvent.getData(PlatformDataKeys.EDITOR);
+    VirtualFile virtualFile = actionEvent
+        .getData(PlatformDataKeys.VIRTUAL_FILE);
+    document = editor.getDocument();
+
+    namespaceFinder = new ParentNamespaceFinder2(project, document, virtualFile, editor);
+
+    namespaceFinder.addResultsReadyListener(new ChangeListener() {
+      @Override
+      public void stateChanged(ChangeEvent changeEvent) {
+        if (changeEvent.getSource().equals("ParentNamespaceFound")) {
+          // TODO: show not found.
+          showDialog();
+        }
+      }
+    });
+
+    namespaceFinder.findParentClass();
+  }
+
+  /**
+   * Show the dialog to select the method to override.
+   */
+  private void showDialog() {
+    List<Function> functionNames = namespaceFinder.getFunctionNames();
+
+    // Select the closest element found from the current position.
+    final JBList jbList = new JBList(functionNames.toArray());
+
+    // Open a pop-up to select which describe() or it() you want to change.
+    JBPopupFactory.getInstance()
+        .createListPopupBuilder(jbList)
+        .setTitle("Select the method to override")
+        .setItemChoosenCallback(new Runnable() {
+          public void run() {
+            if (jbList.getSelectedValue() != null) {
+              addNewMethod((Function) jbList.getSelectedValue());
+            }
+          }
+        })
+        .createPopup()
+        .showCenteredInCurrentWindow(project);
+  }
+
+  /**
+   * Create a new method that will override the parent.
+   *
+   * @param function The method that you want ot override.
+   */
+  private void addNewMethod(Function function) {
+    String fnNameFormat = "%s.prototype." + function.getName();
+    String methodPrototype = String
+        .format(fnNameFormat, namespaceFinder.getCurrentNamespace());
+    String parentMethodPrototype = String
+        .format(fnNameFormat, namespaceFinder.getParentNamespace());
+
+    String arguments = function.getArguments();
+    String callArguments = arguments;
+    if (callArguments.trim().length() > 0) {
+      callArguments = ", " + callArguments;
+    }
+
+    final String methodTemplate = String.format("/**\n" +
+        " * @override\n" +
+        " */\n" +
+        "%s = function(%s) {\n" +
+        "  // TODO: override function\n" +
+        "  %s.call(this%s);\n" +
+        "};\n", methodPrototype, arguments, parentMethodPrototype,
+        callArguments);
+
+    runWriteActionInsideCommand(project, "Override method", new Runnable() {
+      @Override
+      public void run() {
+        int offset = editor.getCaretModel().getOffset();
+        document.replaceString(offset, offset, methodTemplate);
+      }
+    });
   }
 }
---
+++ src/com/karateca/jstoolbox/generatemethod/ParentNamespaceFinder2.java
@@ -0,0 +1,214 @@
+package com.karateca.jstoolbox.generatemethod;
+
+import com.intellij.find.FindManager;
+import com.intellij.find.FindModel;
+import com.intellij.find.FindResult;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ContentIterator;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.EventDispatcher;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * @author Andres Dominguez
+ */
+public class ParentNamespaceFinder2 {
+
+  private final Project project;
+  private final Document document;
+  private final VirtualFile virtualFile;
+  private final Editor editor;
+  private final EventDispatcher<ChangeListener> myEventDispatcher =
+      EventDispatcher.create(ChangeListener.class);
+
+  private FindManager findManager;
+  private String currentNamespace;
+  private String parentNamespace;
+  private List<Function> functionNames;
+
+  public ParentNamespaceFinder2(Project project, Document document,
+      VirtualFile virtualFile, Editor editor) {
+    this.project = project;
+    this.document = document;
+    this.virtualFile = virtualFile;
+    this.editor = editor;
+  }
+
+  public String getCurrentNamespace() {
+    return currentNamespace;
+  }
+
+  public String getParentNamespace() {
+    return parentNamespace;
+  }
+
+  public List<Function> getFunctionNames() {
+    return functionNames;
+  }
+
+  public void findParentClass() {
+    ApplicationManager.getApplication().runReadAction(new Runnable() {
+      @Override
+      public void run() {
+        if (!findParentNamespace()) {
+          return;
+        }
+
+        VirtualFile parentFile = findParentFile();
+        if (parentFile == null) {
+          return;
+        }
+
+        try {
+          functionNames = getMethods(parentFile);
+          broadcastEvent("ParentNamespaceFound");
+        } catch (Exception e) {
+          System.err.println("Error reading file " + virtualFile.getName());
+          e.printStackTrace(System.err);
+        }
+      }
+    });
+  }
+
+  private VirtualFile findParentFile() {
+    final VirtualFile[] parentFile = {null};
+
+    ProjectRootManager.getInstance(editor.getProject()).getFileIndex()
+        .iterateContent(new ContentIterator() {
+          @Override
+          public boolean processFile(VirtualFile virtualFile) {
+            boolean parentFileFound = false;
+            try {
+              parentFileFound = fileProvidesNamespace(virtualFile,
+                  parentNamespace);
+            } catch (IOException e) {
+              System.err.println("Error reading file " + virtualFile.getName());
+              e.printStackTrace(System.err);
+            }
+
+            if (parentFileFound) {
+              parentFile[0] = virtualFile;
+            }
+
+            // Stop the search.
+            return !parentFileFound;
+          }
+        });
+
+    return parentFile[0];
+  }
+
+  /**
+   * Search for the file providing the namespace.
+   */
+  private boolean fileProvidesNamespace(VirtualFile virtualFile,
+      String parentNamespace) throws IOException {
+    String provideSearchLine = "goog.provide('" + parentNamespace;
+    String fileName = virtualFile.getName();
+
+    // Ignore non-js files.
+    if (!fileName.endsWith(".js")) {
+      return false;
+    }
+
+    String fileContents = getFileContents(virtualFile);
+    return fileContents.contains(provideSearchLine);
+  }
+
+  private String getFileContents(VirtualFile virtualFile) throws IOException {
+    return new String(virtualFile.contentsToByteArray());
+  }
+
+  private List<Function> getMethods(VirtualFile virtualFile)
+      throws IOException {
+    List<Function> result = new ArrayList<Function>();
+
+    String fileContents = getFileContents(virtualFile);
+    String regexp = "(%s.prototype.)([\\w]+)(\\s*=\\s*function\\s*\\()" +
+        "([\\w\\s\\n,]*)(\\))";
+    String methodPattern = String.format(regexp, parentNamespace);
+
+    Pattern pattern = Pattern.compile(methodPattern, Pattern.MULTILINE);
+    Matcher matcher = pattern.matcher(fileContents);
+    while (matcher.find()) {
+      String name = matcher.group(2);
+      String arguments = matcher.group(4);
+      result.add(new Function(name, arguments));
+    }
+
+    return result;
+  }
+
+  private TextRange getTextRange(int lineNumber) {
+      int lineStart = document.getLineStartOffset(lineNumber);
+      int lineEnd = document.getLineEndOffset(lineNumber);
+
+      return new TextRange(lineStart, lineEnd);
+    }
+
+  private boolean findParentNamespace() {
+
+    String documentText = document.getText();
+
+    // Find @extends.
+    int extendsOffset = documentText.indexOf("@extends");
+    if (extendsOffset < 0) {
+      return false;
+    }
+
+    // Find the next "*".
+    int lineNumber = document.getLineNumber(extendsOffset);
+    String line = document.getText(getTextRange(lineNumber));
+
+    // Remove the beginning of the line.
+    line = line.replaceAll("[\\s\\*]*@extends\\s+", "");
+    parentNamespace = line.replaceAll("\\s*$", "");
+
+
+
+    return true;
+  }
+
+  private FindModel createFindModel() {
+    findManager = FindManager.getInstance(project);
+    FindModel clone = (FindModel) findManager.getFindInFileModel().clone();
+    clone.setFindAll(true);
+    clone.setFromCursor(true);
+    clone.setForward(true);
+    clone.setMultiline(true);
+    clone.setRegularExpressions(true);
+    clone.setWholeWordsOnly(false);
+    clone.setCaseSensitive(true);
+    clone.setSearchHighlighters(true);
+    clone.setPreserveCase(false);
+
+    return clone;
+  }
+
+  private void broadcastEvent(String eventName) {
+    myEventDispatcher.getMulticaster().stateChanged(new ChangeEvent(eventName));
+  }
+
+  /**
+   * Register for change events.
+   *
+   * @param changeListener The listener to be added.
+   */
+  public void addResultsReadyListener(ChangeListener changeListener) {
+    myEventDispatcher.addListener(changeListener);
+  }
+
+}
