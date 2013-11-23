6eea688002d205f9a2801beed9f008fd6d42d397
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-11-23 18:52:52 -0500

    Add two functions

--- testData/parent.js
+++ testData/parent.js
@@ -5,3 +5,15 @@
 Yo.parent = function () {
 
 };
+
+/**
+ * Do the.
+ * @param {string} a The a.
+ * @param {string} b The a.
+ * @param {string} c The a.
+ */
+Yo.parent.prototype.parentMethod = function (a, b, c) {
+};
+
+Yo.parent.prototype.anotherMethod = function () {
+};
