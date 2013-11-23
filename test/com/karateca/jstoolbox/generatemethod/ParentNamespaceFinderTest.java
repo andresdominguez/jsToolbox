7a1d9b2b5501ee97221eae0ac70c72679c029486
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-11-22 20:47:07 -0500

    new

---
+++ test/com/karateca/jstoolbox/generatemethod/ParentNamespaceFinderTest.java
@@ -0,0 +1,9 @@
+package com.karateca.jstoolbox.generatemethod;
+
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+
+/**
+ * @author Andres Dominguez.
+ */
+public class ParentNamespaceFinderTest extends LightCodeInsightFixtureTestCase {
+}
