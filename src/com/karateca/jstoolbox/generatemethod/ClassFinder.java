3f59b1eeec6ba51e3e0ad9894c2b32ed38588409
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-11-22 19:37:52 -0500

    add methods to find class name and parent

--- src/com/karateca/jstoolbox/generatemethod/ClassFinder.java
+++ src/com/karateca/jstoolbox/generatemethod/ClassFinder.java
@@ -1,26 +1,27 @@
 package com.karateca.jstoolbox.generatemethod;
 
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.TextRange;
 
 public class ClassFinder {
 
   protected final Document document;
+  private final String documentText;
 
   public ClassFinder(Document document) {
     this.document = document;
+    documentText = document.getText();
   }
 
-  protected String findNamespaceForCurrentFile() {
-    String text = document.getText();
-
+  public String getClassName() {
     // Find the @constructor and then go to the first function.
-    int constructorOffset = text.indexOf("@constructor");
+    int constructorOffset = documentText.indexOf("@constructor");
     if (constructorOffset < 0) {
       return null;
     }
 
     // Find the closing jsdoc.
-    int closingJsDocOffset = text.indexOf("*/", constructorOffset);
+    int closingJsDocOffset = documentText.indexOf("*/", constructorOffset);
     if (closingJsDocOffset < 0) {
       return null;
     }
@@ -28,13 +29,40 @@ public class ClassFinder {
     closingJsDocOffset += 2;
 
     // Get the string until the first "=".
-    int indexOfEqual = text.indexOf("=", closingJsDocOffset);
+    int indexOfEqual = documentText.indexOf("=", closingJsDocOffset);
     if (indexOfEqual < 0) {
       return null;
     }
 
     // And remove empty spaces.
-    return text.substring(closingJsDocOffset, indexOfEqual)
+    return documentText.substring(closingJsDocOffset, indexOfEqual)
         .replaceAll("\\s+", "");
+  }
+
+  public String getParentClassName() {
+    // Find @extends.
+    int extendsOffset = documentText.indexOf("@extends");
+    if (extendsOffset < 0) {
+      return null;
+    }
+
+    // Find the next "*".
+    int lineNumber = document.getLineNumber(extendsOffset);
+    String line = document.getText(getTextRange(lineNumber));
+
+    // Remove the beginning of the line.
+    line = line.replaceAll("[\\s\\*]*@extends\\s*\\{?", "");
+
+    // Remove the end of the line.
+    line = line.replaceAll("\\}?\\s*$", "");
+
+    return line;
+  }
+
+  private TextRange getTextRange(int lineNumber) {
+    int lineStart = document.getLineStartOffset(lineNumber);
+    int lineEnd = document.getLineEndOffset(lineNumber);
+
+    return new TextRange(lineStart, lineEnd);
   }
 }
