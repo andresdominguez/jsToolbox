c5212f3cc920a66b91698bd2b32961cb0c7780f6
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-11-25 11:55:27 -0500

    Add getters and setters

--- src/com/karateca/jstoolbox/config/ConfigurationView.java
+++ src/com/karateca/jstoolbox/config/ConfigurationView.java
@@ -1,6 +1,8 @@
 package com.karateca.jstoolbox.config;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author Andres Dominguez.
@@ -14,4 +16,72 @@ public class ConfigurationView {
   private JTextField searchUrl;
   private JCheckBox useFilePath;
   private JTextField fromPath;
+  private JPanel myPanel;
+  private JTextField viewSuffix;
+
+  public ConfigurationView() {
+    useFilePath.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent actionEvent) {
+        fromPath.setEnabled(useFilePath.isSelected());
+      }
+    });
+  }
+
+  public JPanel getMyPanel() {
+    return myPanel;
+  }
+
+  public void setTestSuffix(String testSuffix) {
+    unitTestSuffix.setText(testSuffix);
+  }
+
+  public String getTestSuffix() {
+    return unitTestSuffix.getText();
+  }
+
+  public String getFileSuffix() {
+    return fileSuffix.getText();
+  }
+
+  public void setFileSuffix(String suffix) {
+    fileSuffix.setText(suffix);
+  }
+
+  public String getViewSuffix() {
+    return viewSuffix.getText();
+  }
+
+  public void setViewSuffix(String suffix) {
+    viewSuffix.setText(suffix);
+  }
+
+  public String getSearchUrl() {
+    return searchUrl.getText();
+  }
+
+  public void setSearchUrl(String url) {
+    searchUrl.setText(url);
+  }
+
+  public String getFromPath() {
+    return fromPath.getText();
+  }
+
+  public void setFromPath(String dir) {
+    fromPath.setText(dir);
+  }
+
+  public boolean getUseFilePath() {
+    return useFilePath.isSelected();
+  }
+
+  public void setUseFilePath(boolean selected) {
+    useFilePath.setSelected(selected);
+    fromPath.setEnabled(selected);
+  }
+
+  public JButton getResetButton() {
+    return resetButton;
+  }
 }
