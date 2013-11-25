f269219e7ebd217fb94dac0dc1a91e141a538835
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-11-25 11:51:50 -0500

    new form

---
+++ src/com/karateca/jstoolbox/config/ConfigurationView.java
@@ -0,0 +1,17 @@
+package com.karateca.jstoolbox.config;
+
+import javax.swing.*;
+
+/**
+ * @author Andres Dominguez.
+ */
+public class ConfigurationView {
+  private JPanel goToRelatedPanel;
+  private JPanel openInBrowserPanel;
+  private JButton resetButton;
+  private JTextField unitTestSuffix;
+  private JTextField fileSuffix;
+  private JTextField searchUrl;
+  private JCheckBox useFilePath;
+  private JTextField fromPath;
+}
