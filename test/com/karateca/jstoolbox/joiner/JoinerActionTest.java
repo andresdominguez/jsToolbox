4744a5e16a49ce2cef518211bb667e18e5c8887b
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-19 21:13:13 -0500

    add files to the test

--- test/com/karateca/jstoolbox/joiner/JoinerActionTest.java
+++ test/com/karateca/jstoolbox/joiner/JoinerActionTest.java
@@ -1,7 +1,22 @@
 package com.karateca.jstoolbox.joiner;
 
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+
+import java.io.File;
+
 /**
  * @author Andres Dominguez.
  */
-public class JoinerActionTest {
+public class JoinerActionTest extends LightCodeInsightFixtureTestCase {
+
+  @Override
+  protected String getTestDataPath() {
+    return new File("testData").getPath();
+  }
+
+  public void testJoin() {
+    myFixture.configureByFiles("multiLineStringBefore.js");
+
+    myFixture.checkResultByFile("multiLineStringBefore.js", "multiLineStringAfter.js", false);
+  }
 }
