57cb9c8a3a0afc00b9037f085f823663bb4c95f3
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-19 22:09:04 -0500

    add test files

---
+++ testData/objectToAssignments/objectDeclarationAfter.js
@@ -0,0 +1,8 @@
+var foo = {};
+foo.one = 'xxx';
+foo.two = 123;
+foo.three = 'three';
+foo.person = {
+    name: 'John',
+    lastName: 'Doe'
+};
---
+++ testData/objectToAssignments/objectDeclarationBefore.js
@@ -0,0 +1,9 @@
+var foo = <caret>{
+    one: 'xxx',
+    two: 123,
+    'three': 'three',
+    'person': {
+        name: 'John',
+        lastName: 'Doe'
+    }
+};
