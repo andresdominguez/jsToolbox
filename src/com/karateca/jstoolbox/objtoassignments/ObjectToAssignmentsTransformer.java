29c0d7cd82702684fb49db17d45008fd9518de23
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-21 18:57:06 -0500

    work in progress

--- src/com/karateca/jstoolbox/objtoassignments/ObjectToAssignmentsTransformer.java
+++ src/com/karateca/jstoolbox/objtoassignments/ObjectToAssignmentsTransformer.java
@@ -1,5 +1,7 @@
 package com.karateca.jstoolbox.objtoassignments;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,17 +26,41 @@ public class ObjectToAssignmentsTransformer {
 
     // Start building the string with: "var varName = {".
     int start = objectString.indexOf("{") + 1;
-    sb.append(this.objectString.substring(0, start)).append("}\n");
+    sb.append(objectString.substring(0, start)).append("};\n");
+
+    Pattern nameValuePattern = Pattern.compile("['\"]?(\\w+)['\"]?\\s*:(.+)");
 
     // Transform each variable name.
+    int currentOffset = start;
+    List<Integer> variableLocations = findVariableLocations();
+    String assignment = "";
+
+    for (Integer location : variableLocations) {
+      String assignmentStmt = objectString.substring(currentOffset, location);
+
+      Matcher matcher = nameValuePattern.matcher(assignmentStmt.trim());
+      if (matcher.find()) {
+        String name = matcher.group(1);
+        String value = matcher.group(2).trim();
+
+        if (value.endsWith(",")) {
+          value = value.substring(0, value.length() - 1);
+        }
+
+        assignment = String.format("%s.%s = %s;\n", variableName, name, value);
+      }
+
+      sb.append(assignment);
+
+      currentOffset = location;
+    }
+
 
     return sb.toString();
   }
 
   public String getVariableName() {
-    // Get the variable name by finding all the substring until the first '{'.
-    int braceIndex = objectString.indexOf("{");
-    String substring = objectString.substring(0, braceIndex);
+    String substring = objectString;
 
     Pattern pattern = Pattern.compile("(\\s*\\w*\\s*)(\\w+)(\\s*=\\s*)");
     Matcher matcher = pattern.matcher(substring);
@@ -42,5 +68,20 @@ public class ObjectToAssignmentsTransformer {
       return matcher.group(2);
     }
     return null;
+  }
+
+  private List<Integer> findVariableLocations() {
+    Pattern pattern = Pattern.compile("['\"]?\\w+['\"]?\\s*:.*");
+
+    Matcher matcher = pattern.matcher(objectString);
+
+    List<Integer> locations = new ArrayList<Integer>();
+
+
+    while (matcher.find()) {
+      locations.add(matcher.start());
+    }
+
+    return locations;
   }
 }
