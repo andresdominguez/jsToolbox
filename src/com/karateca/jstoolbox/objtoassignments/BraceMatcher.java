03cfae51cc01943ccd09cdd4b2b3012d8b093436
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-22 00:10:14 -0500

    new

---
+++ src/com/karateca/jstoolbox/objtoassignments/BraceMatcher.java
@@ -0,0 +1,31 @@
+package com.karateca.jstoolbox.objtoassignments;
+
+import java.util.Stack;
+
+/**
+ * @author Andres Dominguez.
+ */
+public class BraceMatcher {
+  public static int getClosingBraceIndex(String codeText, int fromIndex) {
+    Stack<Character> stack = new Stack<Character>();
+
+    int length = codeText.length();
+    for (int i = fromIndex; i < length; i++) {
+      char c = codeText.charAt(i);
+      if (c == '{') {
+        stack.push(c);
+      } else if (c == '}') {
+        if (stack.isEmpty()) {
+          return fromIndex;
+        }
+
+        stack.pop();
+        if (stack.isEmpty()) {
+          return i;
+        }
+      }
+    }
+    // Didn't find anything.
+    return -1;
+  }
+}
