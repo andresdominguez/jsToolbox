7b183f837a2e1342b0e37405a7faea3dd270dc28
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-21 21:20:29 -0500

    introduce getVarNameAndAssignmentValue

--- src/com/karateca/jstoolbox/objtoassignments/ObjectToAssignmentsTransformer.java
+++ src/com/karateca/jstoolbox/objtoassignments/ObjectToAssignmentsTransformer.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
  */
 public class ObjectToAssignmentsTransformer {
   final String objectString;
+  public static final Pattern NAME_VALUE_PATTERN = Pattern.compile("['\"]?(\\w+)['\"]?\\s*:(.+)");
 
   public ObjectToAssignmentsTransformer(String objectString) {
     this.objectString = objectString;
@@ -28,35 +29,40 @@ public class ObjectToAssignmentsTransformer {
     int start = objectString.indexOf("{") + 1;
     sb.append(objectString.substring(0, start)).append("};\n");
 
-    Pattern nameValuePattern = Pattern.compile("['\"]?(\\w+)['\"]?\\s*:(.+)");
-
     // Transform each variable name.
     int currentOffset = start;
     List<Integer> variableLocations = findVariableLocations();
     String assignment = "";
 
     for (Integer location : variableLocations) {
-      String assignmentStmt = objectString.substring(currentOffset, location);
+      assignment = getVarNameAndAssignmentValue(currentOffset, location);
 
-      Matcher matcher = nameValuePattern.matcher(assignmentStmt.trim());
-      if (matcher.find()) {
-        String name = matcher.group(1);
-        String value = matcher.group(2).trim();
-
-        if (value.endsWith(",")) {
-          value = value.substring(0, value.length() - 1);
-        }
-
-        assignment = String.format("%s.%s = %s;\n", variableName, name, value);
-      }
-
+      sb.append(variableName);
       sb.append(assignment);
 
       currentOffset = location;
     }
 
-
     return sb.toString();
+  }
+
+  private String getVarNameAndAssignmentValue(int currentOffset, Integer location) {
+    String assignment;
+    String assignmentStmt = objectString.substring(currentOffset, location);
+
+    assignment = "";
+    Matcher matcher = NAME_VALUE_PATTERN.matcher(assignmentStmt.trim());
+    if (matcher.find()) {
+      String name = matcher.group(1);
+      String value = matcher.group(2).trim();
+
+      if (value.endsWith(",")) {
+        value = value.substring(0, value.length() - 1);
+      }
+
+      assignment = String.format(".%s = %s;\n", name, value);
+    }
+    return assignment;
   }
 
   public String getVariableName() {
@@ -81,6 +87,7 @@ public class ObjectToAssignmentsTransformer {
     while (matcher.find()) {
       locations.add(matcher.start());
     }
+    locations.add(objectString.length() - 1);
 
     return locations;
   }
