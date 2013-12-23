1ec22015412353439d1eca19a9dc9779f9d3983e
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-22 22:25:33 -0500

    add code for the test

---
+++ testData/assignmentsToObject/assignmentsAfter.js
@@ -0,0 +1,19 @@
+/**
+ * The constructor
+ * @constructor
+ * @param first first name.
+ * @param last last name.
+ */
+SomeClass = function (first, last) {
+  this.first = first;
+  this.last = last;
+};
+
+var theThing = {
+  fist: 'Jackie',
+  last: 'Chan',
+  phone: {
+    areacode: 212,
+    phone: '123-4567'
+  }
+};
---
+++ testData/assignmentsToObject/assignmentsBefore.js
@@ -0,0 +1,20 @@
+/**
+ * The constructor
+ * @constructor
+ * @param first first name.
+ * @param last last name.
+ */
+SomeClass = function (first, last) {
+  this.first = first;
+  this.last = last;
+};
+
+<selection>
+var theThing = {};
+theThing.fist = 'Jackie';
+theThing.last = 'Chan';
+theThing.phone = {
+  areacode: 212,
+  phone: '123-4567'
+};
+</selection>
