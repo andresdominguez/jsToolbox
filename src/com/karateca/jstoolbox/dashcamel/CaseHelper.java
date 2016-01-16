118f467f5f094290ae206f89b1cefa8c62fbdbf3
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-01-15 23:37:14 -0500

    Add toCamelCase

---
+++ src/com/karateca/jstoolbox/dashcamel/CaseHelper.java
@@ -0,0 +1,18 @@
+package com.karateca.jstoolbox.dashcamel;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CaseHelper {
+  public static String toCamelCase(String dashCase) {
+    List<String> parts = Arrays.asList(dashCase.split("-"));
+    StringBuilder builder = new StringBuilder(parts.get(0));
+
+    for (String part : parts.subList(1, parts.size())) {
+      builder.append(part.substring(0, 1).toUpperCase());
+      builder.append(part.substring(1));
+    }
+
+    return builder.toString();
+  }
+}
