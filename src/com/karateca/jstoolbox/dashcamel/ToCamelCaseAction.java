75c84a58dfad137a5a79c341c5781ba852176d0d
Billy Jacobson <billy.jacobson@wustl.edu>
2016-01-24 10:43:05 -0500

    Moved "To dash case" and "To camel case" into one action: "Toggle camel and dash case"

--- src/com/karateca/jstoolbox/dashcamel/ToCamelCaseAction.java
+++
@@ -1,8 +0,0 @@
-package com.karateca.jstoolbox.dashcamel;
-
-public class ToCamelCaseAction extends BaseCaseAction {
-
-  String changeCase(String selectedText) {
-    return CaseHelper.toCamelCase(selectedText);
-  }
-}
